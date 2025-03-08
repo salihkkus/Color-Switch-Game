81a81a677a958f4007786785a2fb2d2d92110adb
salihkarakus <sskarakussalih77@gmail.com>
2025-03-08 22:40:03 +0300

    ball controller updated

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -6,10 +6,25 @@ public class BallController : MonoBehaviour
 {
     public float JumpForce;
     public Rigidbody2D rb;
+    public string currentColor;
+    public Color colorcyan;
+    public Color coloryellow;
+    public Color colorpink;
+    public Color colorpurple;
+    public SpriteRenderer sr;
 
     void Start()
     {
+        randomColor();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
         
+        if(col.tag != currentColor)
+        {
+            Debug.Log("Öldün");
+        }
     }
 
     
@@ -18,6 +33,30 @@ public class BallController : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
         {
            rb.velocity = Vector2.up * JumpForce;
+        }
+    }
+
+    void randomColor()
+    {
+        int index = Random.Range(0, 4);
+
+        switch(index)
+        {
+            case 0 : currentColor = "Cyan";
+            sr.color = colorcyan;
+            break;
+
+            case 1 : currentColor = "Yellow";
+            sr.color = coloryellow;
+            break;
+
+            case 2 : currentColor = "Pink";
+            sr.color = colorpink;
+            break;
+
+            case 3 : currentColor = "Purple";
+            sr.color = colorpurple;
+            break;
         }
     }
 }
--- Assets/Scripts/CircleController.cs
+++ Assets/Scripts/CircleController.cs
@@ -6,12 +6,8 @@ public class CircleController : MonoBehaviour
 {
     public float speed;
     public bool direction;
-    public string currentColor;
-    public Color colorcyan;
-    public Color coloryellow;
-    public Color colorpink;
-    public Color colorpurple;
-    public SpriteRenderer sr;
+
+
 
     void Update()
     {
@@ -25,27 +21,6 @@ public class CircleController : MonoBehaviour
         }
     }
 
-    public void randomColor()
-    {
-        int index = Random.Range(0, 4);
-
-        switch(index)
-        {
-            case 0 : currentColor = "cyan";
-            sr.color = colorcyan;
-            break;
-
-            case 1 : currentColor = "yellow";
-            sr.color = coloryellow;
-            break;
-
-            case 2 : currentColor = "pink";
-            sr.color = colorpink;
-            break;
-
-            case 3 : currentColor = "purple";
-            sr.color = colorpurple;
-            break;
-        }
-    }
 }
+
+    
