b6df64c327d6545918a20248e400e40c6f8ad635
salihkarakus <sskarakussalih77@gmail.com>
2025-03-08 22:09:56 +0300

    circle controller updated

--- Assets/Scripts/CircleController.cs
+++ Assets/Scripts/CircleController.cs
@@ -6,6 +6,12 @@ public class CircleController : MonoBehaviour
 {
     public float speed;
     public bool direction;
+    public string currentColor;
+    public Color colorcyan;
+    public Color coloryellow;
+    public Color colorpink;
+    public Color colorpurple;
+    public SpriteRenderer sr;
 
     void Update()
     {
@@ -16,6 +22,30 @@ public class CircleController : MonoBehaviour
         else
         {
             transform.Rotate(0, 0, -1 * speed * Time.deltaTime);
+        }
+    }
+
+    public void randomColor()
+    {
+        int index = Random.Range(0, 4);
+
+        switch(index)
+        {
+            case 0 : currentColor = "cyan";
+            sr.color = colorcyan;
+            break;
+
+            case 1 : currentColor = "yellow";
+            sr.color = coloryellow;
+            break;
+
+            case 2 : currentColor = "pink";
+            sr.color = colorpink;
+            break;
+
+            case 3 : currentColor = "purple";
+            sr.color = colorpurple;
+            break;
         }
     }
 }
