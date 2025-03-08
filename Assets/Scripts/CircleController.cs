921eff073b0c412220a7d93b86cae01246f8f394
salihkarakus <sskarakussalih77@gmail.com>
2025-03-08 21:57:15 +0300

    circle controller added and updated

---
+++ Assets/Scripts/CircleController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleController : MonoBehaviour
+{
+    public float speed;
+    public bool direction;
+
+    void Update()
+    {
+        if(direction == false)
+        {
+            transform.Rotate(0, 0, speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Rotate(0, 0, -1 * speed * Time.deltaTime);
+        }
+    }
+}
